05762f3a56a8d84ea0c6b3e65d6ef71d8408fe21
Unknown <khaldi.selim@gmail.com>
2017-11-27 19:15:44 +0100

    First Controller

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 Louvre
+=================
+
+This is my 4th school project. It is based on **Symfony 4.1**.
+
---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * Author: Selim Khaldi
+ * Author URI: https://twitter.com/SelimKhaldi
+ * Company: KHALSOMM Digital
+ * Company URI: http://www.khalsomm.com
+ * Date: 27/11/2017
+ * Time: 18:51
+ */
+
+namespace App\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Response;
+
+class DefaultController extends Controller
+{
+    /**
+     * @return Response
+     * @Route("/", name="index")
+     * @Method("GET")
+     */
+    public function indexAction()
+    {
+        return $this->render('Default/index.html.twig');
+    }
+}
