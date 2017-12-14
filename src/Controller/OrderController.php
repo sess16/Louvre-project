da496449d11f41c96a7f44443947edc0c234e244
Unknown <khaldi.selim@gmail.com>
2017-12-14 14:15:20 +0100

    Start database arch

--- src/Controller/OrderController.php
+++ src/Controller/OrderController.php
@@ -12,22 +12,23 @@
 namespace App\Controller;
 
 
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class OrderController extends Controller
 {
+    const MAXIMUM_TICKET = 1000;
+
     /**
      * @return Response
      * @Route("/create", name="create")
-     * @Method("GET")
      */
     public function create()
     {
         return $this->render('Default/create.html.twig');
     }
+
 
 
 }
